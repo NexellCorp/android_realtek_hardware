efde3606a4e245f63288bd3a914a874ccc89534f
Sungwoo Park <swpark@nexell.co.kr>
2014-12-31 11:14:17 +0900

    [APPLY] slsiap

--- wlan/driver/rtl8188EUS_linux_v4.3.0.3_10997.20140327/build.sh
+++ wlan/driver/rtl8188EUS_linux_v4.3.0.3_10997.20140327/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 make ARCH=arm clean
 make ARCH=arm modules -j4
-cp wlan.ko ../../../../../hardware/nexell/pyrope/prebuilt/modules/
+cp wlan.ko ../../../../../hardware/samsung_slsi/slsiap/prebuilt/modules/
+
